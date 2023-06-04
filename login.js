268fdd1b09fef05dec10a9a30bb33f847920fa7c
yashuvaishnav <yashurishu22@gmail.com>
2023-06-04 16:08:39 +0530

    Done work

---
+++ login.js
@@ -0,0 +1,25 @@
+
+let userData = JSON.parse(localStorage.getItem("account-data")) || [];
+  document.querySelector("form").addEventListener("submit", userLogin);
+
+  function userLogin(e) {
+    e.preventDefault();
+    let heading = document.getElementById("heading");
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let data = userData.find(
+      (user) => user.email === email && user.password === password
+    );
+   
+      if (data) {
+        alert("Sign in Successful");
+        window.location.href = "index.html";
+      }
+      else {
+        alert("Wrong Credentials");
+        location.reload();
+      }
+
+    }
+  
